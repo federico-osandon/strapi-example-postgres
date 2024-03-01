14da7bb8b2c1237b39d3256957570ccf57556e1c
Federico Osandon <defe014@gmail.com>
2024-03-01 18:12:37 +0100

    first commit

---
+++ src/api/viedeogame/routes/viedeogame.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * viedeogame router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::viedeogame.viedeogame');
---
+++ src/api/viedeogame/services/viedeogame.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * viedeogame service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::viedeogame.viedeogame');
